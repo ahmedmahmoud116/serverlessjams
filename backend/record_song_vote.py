b1e6aede991873ed1f7d9625bd9a59464ae83cc7
ahmedmahmoud116 <ahmedmahmoud116@ahmed.mahmoud8116@gmail.com>
2021-08-25 16:34:14 +0200

    implement done

--- backend/record_song_vote.py
+++ backend/record_song_vote.py
@@ -2,9 +2,12 @@ import boto3
 import os
 import json
 
-dynamodb = boto3.client('dynamodb');
+from scope_check import scope_check
+
+dynamodb = boto3.client('dynamodb')
 
 def handler(event, context):
+    scope_check(event, required_scopes='write:votes')
     song_name = json.loads(event['body'])['songName'];
     result = dynamodb.update_item(
         TableName = os.environ['DYNAMODB_TABLE'], #Must specify the tablename you are interacting with
